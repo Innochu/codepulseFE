6aa58d02fc5ff65f3a2c16e3d7067dac41f0e1c2
CHUKWUDI INNOCENT <125125776+Innochu@users.noreply.github.com>
2023-11-22 02:24:30 +0100

    worked on my service for get all users and created a table to house results from my api, i have not yet consumed the api

--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Category } from '../models/category.model';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +15,11 @@ export class CategoryService {
 
 
   addCategory(model: AddCategoryRequest) : Observable<void> {
+   
+return this.http.post<void>(`${environment.apiBaseUrl}/api/Categories/Add-Category`, model)
+  };
 
-return this.http.post<void>('https://localhost:7278/api/categories', model)
-  }
+   getAllCategories(): Observable<Category[]>{
+     return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/Categories/Get-All`)
+  };
 }
